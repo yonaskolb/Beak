53f29984f600a1ed9c6dbb3fe141875f061e1f33
Yonas Kolb <yonaskolb@gmail.com>
2018-01-04 15:23:42 +0800

    add homebrew formula

---
+++ Formula/beak.rb
@@ -0,0 +1,12 @@
+class Beak < Formula
+  desc "A command line interface for your Swift scripts"
+  homepage "https://github.com/yonaskolb/Beak"
+  url "https://github.com/yonaskolb/Beak/archive/0.2.0.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  head "https://github.com/yonaskolb/Beak.git"
+
+  depends_on :xcode
+
+  def install
+    system "swift run beak run install"
+  end
