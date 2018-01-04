805d3541f94fac437bb3e423692509cc98882477
Yonas Kolb <yonaskolb@gmail.com>
2018-01-04 15:23:56 +0800

    beak file: added updateBrew task

--- beak.swift
+++ beak.swift
@@ -7,6 +7,9 @@ import SwiftShell
 import Regex
 import PathKit
 
+let tool = "beak"
+let repo = "https://github.com/yonaskolb/Beak"
+
 /// Formats all the code in the project
 public func formatCode() throws {
     let formatOptions = "--wraparguments beforefirst --stripunusedargs closure-only --header strip"
@@ -25,8 +28,35 @@ public func install(directory: String = "/usr/local/bin") throws {
         print("Couldn't build:\n\(error)")
         return
     }
-    try runAndPrint(bash: "cp -R .build/release/beak \(directory)/beak")
+    try runAndPrint(bash: "cp -R .build/release/\(tool) \(directory)/\(tool)")
     print("🐦  Installed Beak!")
+}
+
+/// Updates homebrew formular to a certain version
+///
+/// - Parameters:
+///   - version: The version to release
+public func updateBrew(_ version: String) throws {
+  let releaseTar = "\(repo)/archive/\(version).tar.gz"
+  let output = run(bash: "curl -L -s $(RELEASE_TAR) | shasum -a 256 | sed 's/ .*//'")
+  guard output.succeeded else {
+    print("Error retrieving brew SHA")
+    return
+  }
+  let sha = output.stdout
+
+  try replaceFile(
+      regex: "(url \".*/archive/)(.*).tar.gz",
+      replacement: "$1\(version).tar.gz",
+      path: "Formula/beak.rb")
+
+  try replaceFile(
+      regex: "sha256 \".*\"",
+      replacement: "sha256 \"\(sha)\"",
+      path: "Formula/beak.rb")
+
+  //run(bash: "git add Formula/beak.rb")
+  //run(bash: "git commit -m \"Updated brew to \(version)\"")
 }
 
 /// Releases a new version of Beak
@@ -34,6 +64,7 @@ public func install(directory: String = "/usr/local/bin") throws {
 /// - Parameters:
 ///   - version: The version to release
 public func release(_ version: String) throws {
+
     try replaceFile(
         regex: "public let version: String = \".*\"",
         replacement: "public let version: String = \"\(version)\"",
@@ -42,7 +73,7 @@ public func release(_ version: String) throws {
     run(bash: "git add Sources/BeakCore/Beak.swift")
     run(bash: "git commit -m \"Updated to \(version)\"")
     run(bash: "git tag \(version)")
-    
+
     print("🐦  Released version \(version)!")
 }
 
