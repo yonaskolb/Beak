bb9d175282e341a7dcb189d2151bbf6777975144
Yonas Kolb <yonaskolb@gmail.com>
2018-01-05 14:20:41 +0800

    simplify main.swift

--- Sources/Beak/main.swift
+++ Sources/Beak/main.swift
@@ -3,21 +3,15 @@ import Foundation
 import PathKit
 import Utility
 
-func tryCommand(_ closure: () throws -> Void) {
-    do {
-        try closure()
-    } catch {
-        if error._domain == NSCocoaErrorDomain {
-            print("⚠️  \(error.localizedDescription)")
-        } else {
-            print("⚠️  \(error)")
-        }
-        exit(1)
-    }
-}
-
-let options = BeakOptions()
-let beak = Beak(options: options)
-tryCommand {
+do {
+    let options = BeakOptions()
+    let beak = Beak(options: options)
     try beak.execute(arguments: Array(ProcessInfo.processInfo.arguments.dropFirst()))
+} catch {
+    if error._domain == NSCocoaErrorDomain {
+        print("⚠️  \(error.localizedDescription)")
+    } else {
+        print("⚠️  \(error)")
+    }
+    exit(1)
 }
