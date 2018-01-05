ca876ed4ecfa6e1c50b19f5514b7792aaf1d771b
Yonas Kolb <yonaskolb@gmail.com>
2018-01-05 22:05:17 +0800

    Updated brew to 0.3.0

--- Formula/beak.rb
+++ Formula/beak.rb
@@ -1,8 +1,8 @@
 class Beak < Formula
   desc "A command line interface for your Swift scripts"
   homepage "https://github.com/yonaskolb/Beak"
-  url "https://github.com/yonaskolb/Beak/archive/0.2.0.tar.gz"
-  sha256 "76f7c837ed9d393e7de24173dd728b73d9d5b5e5a923f6c2c1d50be4ee105426"
+  url "https://github.com/yonaskolb/Beak/archive/0.3.0.tar.gz"
+  sha256 "eff35feb43563876689dbcc19aa22142e5f717f86e9565fb699964c1f373c8c2"
   head "https://github.com/yonaskolb/Beak.git"
 
   depends_on :xcode
