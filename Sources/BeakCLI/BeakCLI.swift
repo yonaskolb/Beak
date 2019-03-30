17a35b6094a3989a5f030185f8687232e3c4cab2
yonaskolb <yonaskolb@gmail.com>
2019-03-30 17:12:21 +1100

    Updated to 0.5.0

--- Sources/BeakCLI/BeakCLI.swift
+++ Sources/BeakCLI/BeakCLI.swift
@@ -4,7 +4,7 @@ import Foundation
 
 public class BeakCLI {
 
-    public let version: String = "0.4.0"
+    public let version: String = "0.5.0"
     public let options: BeakOptions
 
     public init(options: BeakOptions) {
